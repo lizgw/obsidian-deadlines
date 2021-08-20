884edfb56f7d1f3fecd169d834e83d37ea8139a3
Liz Wigglesworth <liz.wigglesworth@gmail.com>
2021-08-19 19:32:10 -0500

    Display deadlines on calendar with correct dates

--- src/DeadlineView.ts
+++ src/DeadlineView.ts
@@ -35,8 +35,6 @@ export default class DeadlineView extends ItemView {
     // let today = new Date(2021, 7, 8);
     let today = new Date();
     let todayMonthStr = today.toLocaleString("default", { month: "long" });
-    let todayDayNum = today.getDate();
-    // let todayWeekday = today.toLocaleString("default", {weekday: "short"});
     let todayWeekdayNum = today.getDay(); // 0 = sunday
     // in case the current day isn't a monday, we need to shift it over
     // so we need to figure out which day to start on
@@ -71,13 +69,33 @@ export default class DeadlineView extends ItemView {
       }
     }
 
-    // render all the deadlines
-    this.createDeadlineBlock(this.deadlineData[0], this.getNthDayBlock(0));
-    this.createDeadlineBlock(this.deadlineData[1], this.getNthDayBlock(1));
-    this.createDeadlineBlock(this.deadlineData[2], this.getNthDayBlock(1));
-    this.createDeadlineBlock(this.deadlineData[3], this.getNthDayBlock(2));
-    
+    this.renderDeadlines();
   }
+
+  renderDeadlines() {
+    console.log(this.deadlineData);
+    let skipping = false;
+    // render all the deadlines
+    this.deadlineData.forEach(dl => {
+      // TODO: skip dates before current week
+      // only render dates that aren't "done"
+      if (!skipping && dl.status != "done") {
+        // find the block to add it to
+        let dateStr = this.dateToFormatString(dl.date);
+        // console.log(dateStr + "for date " + dl.date.toUTCString());
+        let block = document.getElementById(dateStr);
+
+        // if this date isn't on the calendar, then stop
+        // because we've gone too far into the future
+        if (block != null) {
+          this.createDeadlineBlock(dl, block);
+        } else {
+          skipping = true;
+        }
+      }
+    });
+  }
+
   getDisplayText() {
     return "Deadlines";
   }
@@ -121,6 +139,7 @@ export default class DeadlineView extends ItemView {
       text: "" + date.getDate(),
       cls: "calendar-day-block-number"
     });
+    block.setAttribute("id", this.dateToFormatString(date));
 
     // add a class to the weekends
     // TODO: make a toggle setting for this
@@ -146,6 +165,24 @@ export default class DeadlineView extends ItemView {
     })
 
     return block;
+  }
+
+  // this deals in local time!
+  dateToFormatString(date: Date) {
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+    let day = date.getDate();
+
+    let result = `${year}-`;
+    if (month < 10) {
+      result += "0";
+    }
+    result += month + "-";
+    if (day < 10) {
+      result += "0";
+    }
+    result += day;
+    return result;
   }
 
   createDeadlineBlock(deadline: Deadline, calBlock: Element) {
@@ -177,9 +214,20 @@ export default class DeadlineView extends ItemView {
       let cacheInfo = this.app.metadataCache.getFileCache(file);
       // only process files with a deadline in the frontmatter
       if (cacheInfo.frontmatter && cacheInfo.frontmatter.deadline != undefined) {
+        // we have to copy the date to a new object here
+        // reading the date from the file creates a new date on that day at 00:00 UTC
+        // which can become a different day in local time
+        // so we need to make a day based on now in local time
+        // and then copy the date data over from the date in the frontmatter
+        // dates are weird and this sucks
+        let utcDate = new Date(cacheInfo.frontmatter.deadline);
+        let localDate = new Date();
+        localDate.setFullYear(utcDate.getUTCFullYear());
+        localDate.setMonth(utcDate.getUTCMonth());
+        localDate.setDate(utcDate.getUTCDate());
         data.push(new Deadline(
           file.basename,
-          new Date(cacheInfo.frontmatter.deadline),
+          localDate,
           cacheInfo.frontmatter.group,
           cacheInfo.frontmatter.status,
           file
